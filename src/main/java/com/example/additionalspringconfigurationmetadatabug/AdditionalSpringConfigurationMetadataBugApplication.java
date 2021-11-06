90ff750ebe0b99bd16fecdb2141e8cc898f7879b
vaa25 <alex.vlasov.ua@gmail.com>
2021-11-06 13:23:00 +0200

    Initial commit

---
+++ src/main/java/com/example/additionalspringconfigurationmetadatabug/AdditionalSpringConfigurationMetadataBugApplication.java
@@ -0,0 +1,13 @@
+package com.example.additionalspringconfigurationmetadatabug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionalSpringConfigurationMetadataBugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdditionalSpringConfigurationMetadataBugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/additionalspringconfigurationmetadatabug/AdditionalSpringConfigurationMetadataBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.additionalspringconfigurationmetadatabug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditionalSpringConfigurationMetadataBugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
